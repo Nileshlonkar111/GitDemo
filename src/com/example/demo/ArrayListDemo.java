18854be5eecb1f5b765d25e9343603d2b6812ee1
nilesh <nileshlonkar111@gmail.com>
2022-12-24 19:17:58 +0530

    First code is ArrayList

---
+++ src/com/example/demo/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList();
+		list.add("Nilesh");
+		list.add("Nutan");
+		list.add("Nikhil");
+		System.out.println(list);
+
+	}
+}
